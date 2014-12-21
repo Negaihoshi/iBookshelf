7aa535befe1de7f35c80552da6161a61dd75ef03
gosick <f56112000@gmail.com>
2014-12-21 18:44:33 +0800

    fix request

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,18 +1,31 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class RegisterController extends Controller {
 
+      protected $rules = array(
+        'username'    => 'required|username',
+        'email'       => 'required|email',
+        'password'    => 'required|min:6'
+      );
 
       public function index()
       {
         return view('Register');
       }
 
-      public function registerMember()
+      public function registerMember(Request $request)
       {
-        $name = Input::get('name');
-        $email = Input::get('email');
-        $password = Input::get('password');
-        $password = Hash::make('secret');
+        $username = $request->input('username');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $data = array(
+          'username'    => $username,
+          'email'       => $email,
+          'password'    => $password,
+          'verify'      => 'true'
+        );
+        return $data;
       }
 }
